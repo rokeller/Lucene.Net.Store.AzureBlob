c4da5ba9e3ec4ffbdd85b05b86649cdcfb9043e3
Roger Keller <rokeller@cymbeline.ch>
2021-05-29 14:12:46 +0200

    Remove unused namespaces.

--- src/Lucene.Net.Store.AzureBlob/FileBackedAzureBlobDirectory.cs
+++ src/Lucene.Net.Store.AzureBlob/FileBackedAzureBlobDirectory.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 using Lucene.Net.Index;
 using Microsoft.Azure.Storage;
 using Microsoft.Azure.Storage.Blob;
